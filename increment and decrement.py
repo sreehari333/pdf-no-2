cdd972c1796c6d3e866c28b345d08a3c98d83fed
sreehari333 <85100069+sreehari333@users.noreply.github.com>
2021-05-31 20:40:46 +0530

    Add files via upload

---
+++ increment and decrement.py
@@ -0,0 +1,17 @@
+#this program is for increment
+
+
+count = 0
+count += 1
+count = count + 1
+print('The Value of Count is', count)
+
+print("INCREMENTED FOR LOOP")
+for i in range(0, 5):
+    print(i)
+
+# this program is for decrement
+
+print("\n DECREMENTED FOR LOOP")
+for i in range(4, -1, -1):
+   print(i)
