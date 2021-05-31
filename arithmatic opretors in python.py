daf31f92f8107815b7eec189a84da47c519dd032
sreehari333 <85100069+sreehari333@users.noreply.github.com>
2021-05-31 20:35:23 +0530

    Add files via upload

---
+++ arithmatic opretors in python.py
@@ -0,0 +1,19 @@
+a = 21
+b = 10
+c = 0
+
+#program for +
+c = a + b
+print("Line 1 - Value of c is ", c)
+
+#program for -
+c = a - b
+print("Line 2 - value of c is", c)
+
+#program for *
+c = a * b
+print("line 3 - value of c is ", c)
+
+#program for /
+c = a / b
+print("Line 4 - Value of c is ", c)
