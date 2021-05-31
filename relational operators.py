3ce347ea47008abad412a7e9b7b5dc26386bb11b
sreehari333 <85100069+sreehari333@users.noreply.github.com>
2021-05-31 20:58:24 +0530

    Add files via upload

---
+++ relational operators.py
@@ -0,0 +1,13 @@
+a, b = 10, 20
+
+print(a == b) # since the values 10 and 20 are different, the output of this print statement is False
+
+print(a != b) # since both the values are not equal, the output of this print statement is True
+
+print(a > b) # the value 10 is less than 20, hence the output of this print statement is False
+
+print(a < b) # since the value 10 is less than 20, the output of this print statement is True
+
+print(a >= b) # Since the value 10 is less than 20, the output of this print statement is False
+
+print(a <= b) # Since the value 10 is less than 20, the output of this print statement is True
